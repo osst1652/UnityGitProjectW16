0f5388fda44bb67486c5c9b44305bb90830e5ec8
osst1652 <oskar.steinhauf@gmail.com>
2017-04-19 22:43:23 +0200

    fixed bugs + started on first scene
    Fixed the bugs in the code and made some adjustments. Also started on
    the first level prototype.

--- UnityWGithubw16/Assets/Scripts/PlayerMovement.cs
+++ UnityWGithubw16/Assets/Scripts/PlayerMovement.cs
@@ -1,90 +1,100 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-    public Animator anim;
+public class PlayerMovement : MonoBehaviour
+{
+
     private int Horizontal = 0;
+
+    public Animator anim;
     public float MovementSpeed = 5f;
     public float Gravity = 1f;
     public CharacterController charController;
-    private FacingDirection _playerFacingDirection;
-    public float jumpHeight = 0f;
-    public bool isJumping = false;
+    private FacingDirection _myFacingDirection;
+    public float JumpHeight = 0f;
+    public bool _jumping = false;
     private float degree = 0;
-    
-    public FacingDirection cmdFacingDirection
-    {
-        set { _playerFacingDirection = value; }
-    }
 
 
-    private void MoveCharacter(float moveFactor)
+    public FacingDirection CmdFacingDirection
     {
-        Vector3 transition = Vector3.zero;
-        if(_playerFacingDirection == FacingDirection.Front)
+
+        set
         {
-            transition = new Vector3(Horizontal * moveFactor, -Gravity * moveFactor, 0f);
+            _myFacingDirection = value;
         }
-        else if(_playerFacingDirection == FacingDirection.Right)
-        {
-            transition = new Vector3(0f, -Gravity * moveFactor, Horizontal * moveFactor);
-        }
-        else if(_playerFacingDirection == FacingDirection.Back)
-        {
-            transition = new Vector3(-Horizontal * moveFactor, -Gravity * moveFactor, 0f);
-        }
-        else if(_playerFacingDirection == FacingDirection.Left)
-        {
-            transition = new Vector3(0f, -Gravity * moveFactor, -Horizontal * moveFactor);
-        }
-        if (isJumping)
-        {
-            transform.Translate(Vector3.up * jumpHeight * Time.deltaTime);
-        }
-        charController.SimpleMove(transition);
+
     }
 
-	public IEnumerator JumpingWait()
+    // Update is called once per frame
+    void Update()
     {
-        yield return new WaitForSeconds(0.35f);
-        isJumping = false;
-    }
-	// Update is called once per frame
-	void Update () {
 
         if (Input.GetAxis("Horizontal") < 0)
-        {
             Horizontal = -1;
-        }else if (Input.GetAxis("Horizontal") > 0)
-        {
+        else if (Input.GetAxis("Horizontal") > 0)
             Horizontal = 1;
-        }
         else
-        {
             Horizontal = 0;
-        }
 
-        if(Input.GetKeyDown(KeyCode.Space)&& !isJumping)
+        if (Input.GetKeyDown(KeyCode.Space) && !_jumping)
         {
-            isJumping = true;
+            _jumping = true;
             StartCoroutine(JumpingWait());
         }
+
         if (anim)
         {
             anim.SetInteger("Horizontal", Horizontal);
+
             float moveFactor = MovementSpeed * Time.deltaTime * 10f;
-            //MoveCharacter
             MoveCharacter(moveFactor);
 
         }
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, degree, 0), 8 * Time.deltaTime);
-		
 
-	}
-    public void updateToFacingdirection(FacingDirection newDirection, float angle)
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, degree, 0), 8 * Time.deltaTime);
+
+    }
+
+    private void MoveCharacter(float moveFactor)
     {
-        _playerFacingDirection = newDirection;
+        Vector3 trans = Vector3.zero;
+        if (_myFacingDirection == FacingDirection.Front)
+        {
+            trans = new Vector3(Horizontal * moveFactor, -Gravity * moveFactor, 0f);
+        }
+        else if (_myFacingDirection == FacingDirection.Right)
+        {
+            trans = new Vector3(0f, -Gravity * moveFactor, Horizontal * moveFactor);
+        }
+        else if (_myFacingDirection == FacingDirection.Back)
+        {
+            trans = new Vector3(-Horizontal * moveFactor, -Gravity * moveFactor, 0f);
+        }
+        else if (_myFacingDirection == FacingDirection.Left)
+        {
+            trans = new Vector3(0f, -Gravity * moveFactor, -Horizontal * moveFactor);
+        }
+        if (_jumping)
+        {
+            transform.Translate(Vector3.up * JumpHeight * Time.deltaTime);
+        }
+
+
+        charController.SimpleMove(trans);
+    }
+    public void UpdateToFacingDirection(FacingDirection newDirection, float angle)
+    {
+
+        _myFacingDirection = newDirection;
         degree = angle;
+
+    }
+
+    public IEnumerator JumpingWait()
+    {
+        yield return new WaitForSeconds(0.35f);
+        //Debug.Log ("Returned jump to false");
+        _jumping = false;
     }
 }
